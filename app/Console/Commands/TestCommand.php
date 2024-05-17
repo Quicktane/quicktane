365911b89f4e2380476bfb863ffa7c9d8c1f455d
Andriy Hrechyn <andriy.hrechyn@gmail.com>
2024-05-17 11:22:04 +0200

    added prices to product

--- app/Console/Commands/TestCommand.php
+++ app/Console/Commands/TestCommand.php
@@ -3,9 +3,9 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
-use Quicktane\Core\Product\Dto\AttributeDto;
-use Quicktane\Core\Product\Dto\AttributeGroupDto;
-use Quicktane\Core\Product\Enums\AttributeType;
+use Quicktane\Core\Product\Dto\ProductDto;
+use Quicktane\Core\Product\Enums\ProductType;
+use Quicktane\Core\Product\Models\Price;
 use Quicktane\Core\Product\Services\AttributeGroupService;
 use Quicktane\Core\Product\Services\AttributesService;
 use Quicktane\Core\Product\Services\ProductService;
@@ -29,39 +29,69 @@ class TestCommand extends Command
     /**
      * Execute the console command.
      */
-    public function handle(ProductService $productService, AttributesService $attributesService, AttributeGroupService $attributeGroupService)
-    {
-        $attribute = $attributesService->create(AttributeDto::fromArray([
-            'name' => 'Name',
-            'slug' => 'name',
-            'type' => AttributeType::STRING,
-        ]));
-
-        $attribute1 = $attributesService->create(AttributeDto::fromArray([
-            'name' => 'Description',
-            'slug' => 'description',
-            'type' => AttributeType::STRING,
-        ]));
-
-        $attributeGroup = $attributeGroupService->create(AttributeGroupDto::fromArray([
-            'name' => 'Default',
-            'slug' => 'default',
-            'attributes' => [$attribute->id, $attribute1->id]
-        ]));
-
-//        $product = $productService->create(ProductDto::fromArray([
-//            'attribute_group' => 1,
-//            'type'            => ProductType::SIMPLE,
-//            'sku'             => 'qwe',
-//            'quantity'        => 3,
-//            'attributes'      => [
-//                'name'        => 'My new product',
-//                'description' => 'My new product description',
-//                'width'       => 1,
-//                'height'      => 1,
-//                'length'      => 1,
-//            ],
+    public function handle(
+        ProductService $productService,
+        AttributesService $attributesService,
+        AttributeGroupService $attributeGroupService
+    ) {
+//        $attribute = $attributesService->create(AttributeDto::fromArray([
+//            'name' => 'Name',
+//            'slug' => 'name',
+//            'type' => AttributeType::STRING,
 //        ]));
+//
+//        $attribute1 = $attributesService->create(AttributeDto::fromArray([
+//            'name' => 'Description',
+//            'slug' => 'description',
+//            'type' => AttributeType::STRING,
+//        ]));
+//
+//        $attributeGroup = $attributeGroupService->create(AttributeGroupDto::fromArray([
+//            'name' => 'Default',
+//            'slug' => 'default',
+//            'attributes' => [$attribute->id, $attribute1->id]
+//        ]));
+
+//        Currency::create([
+//            'code' => 'USD',
+//            'name' => 'US Dollar',
+//            'exchange_rate' => 1,
+//            'decimal_places' => 2,
+//            'default' => true,
+//            'enabled' => true,
+//        ]);
+
+//                Currency::create([
+//            'code' => 'EUR',
+//            'name' => 'EUR',
+//            'exchange_rate' => 1,
+//            'decimal_places' => 2,
+//            'default' => false,
+//            'enabled' => true,
+//        ]);
+
+        $product = $productService->create(ProductDto::fromArray([
+            'attribute_group' => 1,
+            'type'            => ProductType::SIMPLE,
+            'sku'             => 'qwe',
+            'quantity'        => 3,
+            'attributes'      => [
+                'name' => 'My new product',
+                'description' => 'My new product description',
+                'width' => 1,
+                'height' => 1,
+                'length' => 1,
+            ],
+        ]));
+
+        $price = Price::query()->newModelInstance();
+        $price->product()->associate($product);
+        $price->amount = money(100, 'EUR');
+        $price->save();
+
+        $prices = $product->prices;
+
+        dd($prices);
 
 //        $product = $productService->update(Product::query()->find(1), ProductDto::fromArray([
 //            'attribute_group' => 1,
